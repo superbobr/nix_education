d0d7b1d36755a52d679a173d37754f83d10d83ea
superbobr <leonseoprofi@gmail.com>
2025-01-11 14:13:12 +0200

    add: initial profile.php

---
+++ pages/profile.php
@@ -0,0 +1,10 @@
+<?php include '../components/header.php'; ?>
+<?php include '../components/navigation.php'; ?>
+
+<main>
+    <h2>Profile</h2>
+    <p>Welcome, [username]!</p>
+    <p>Email: [user@example.com]</p>
+</main>
+
+<?php include '../components/footer.php'; ?>
