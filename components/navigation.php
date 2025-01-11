3a532b5b2d40a6eeb194cead9bbbf55ce95ea6dc
superbobr <leonseoprofi@gmail.com>
2025-01-11 13:53:55 +0200

    add: initial navigation.php

---
+++ components/navigation.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="/pages/login.php">Login</a></li>
+        <li><a href="/pages/register.php">Register</a></li>
+        <li><a href="/pages/posts.php">Posts</a></li>
+        <li><a href="/pages/profile.php">Profile</a></li>
+    </ul>
+</nav>
